c6c24c50363bf337dbdf556c8d0052419a9bb403
gabbytb <igabriel.oyebanji@travelbeta.com>
2024-10-30 13:51:17 +0100

    1. SIGN IN WITH GOOGLE WORKING PERFECTLY... 2. ADMIN DASHBOARD: USER, BLOG SINGLE POST PG  ''-n-'' STAFF PAGE WORKING PERFECTLY - Pushed to Repository (vybezonvibes@protonmail.com)placeholder-gray-500

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 const api = axios.create({
     // baseURL: 'http://192.168.2.113:8000', // This is your server’s base URL
     baseURL: 'http://localhost:8000', // This is your server’s base URL
-    timeout: 38000, // Optional: Maximum time to wait for a response
+    timeout: 50000, // Optional: Maximum time to wait for a response
 });
 
 export default api;
