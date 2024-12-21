3689c2345a9925f9b1556e65d855e4c8ea295965
gabbytb <igabriel.oyebanji@travelbeta.com>
2024-12-21 11:05:23 +0100

    Successfully merged backend and frontend together

--- models/donation.model.js
+++ models/donation.model.js
@@ -3,71 +3,71 @@ module.exports = mongoose => {
     const { Schema } = mongoose;
 
     const donationSchema = new Schema({
-    _id: {
-        type: Number
-    },
-    images: [
-        { 
-            _id: { 
-                type: Number,                   
-            },
-            url: { 
-                type: String,
-            }, 
-            featured: { 
-                type: Boolean,
-            }, 
-            createdAt: { 
-                type: Date,
-            },
-            updatedAt: { 
-                type: Date,
-            },
+        _id: {
+            type: Number
         },
-    ],
-    title: {
-        type: String,
-    },
-    uri: {
-        type: String,
-    },
-    description: {
-        type: String,
-    },   
-    excerpt: {
-        type: String,
-    },
-    author: {
-        type: String,             
-    },
-    donor: [
-        {
-            contribution: Number,
-            userName: { 
-                type: String, 
-                default: 'anonymous',
-            },         
-            company: String,        
-            email: String,
-            Phone: Number,
+        images: [
+            { 
+                _id: { 
+                    type: Number,                   
+                },
+                url: { 
+                    type: String,
+                }, 
+                featured: { 
+                    type: Boolean,
+                }, 
+                createdAt: { 
+                    type: Date,
+                },
+                updatedAt: { 
+                    type: Date,
+                },
+            },
+        ],
+        title: {
+            type: String,
         },
-    ],
-    tags: [],
-    categories: [],
-    status: { 
-        type: String, 
-        default: 'draft',
-    },
-    isPublished: { 
-        type: Boolean, 
-        default: false,
-    },
-    accessToken: {
-        type: String,
-    },    
+        uri: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },   
+        excerpt: {
+            type: String,
+        },
+        author: {
+            type: String,             
+        },
+        donor: [
+            {
+                contribution: Number,
+                userName: { 
+                    type: String, 
+                    default: 'anonymous',
+                },         
+                company: String,        
+                email: String,
+                Phone: Number,
+            },
+        ],
+        tags: [],
+        categories: [],
+        status: { 
+            type: String, 
+            default: 'draft',
+        },
+        isPublished: { 
+            type: Boolean, 
+            default: false,
+        },
+        accessToken: {
+            type: String,
+        },
     });
 
-    const Donations = mongoose.Model('Donations', donationSchema);
+    const Donations = mongoose.model('Donations', donationSchema);
     return Donations;
 
 };
