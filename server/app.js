d917f24123f9d2945fa78babe46bbc32978d4f2e
Gabby <igabrieloyebanji@protonmail.com>
2025-01-26 00:50:52 +0100

    CHECK THIS COMMIT OUT

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const CSPort = 3000,
       port = process.env.PORT || portie;
 const corsOptions = {
     // origin:`http://localhost:${portie}`,    // Set origin of client-side IP
-    origin: 'https://679578297cf73af1da94976b--superlative-crepe-cc644f.netlify.app',
+    origin: 'https://679578b57cf73af18c9497d9--superlative-crepe-cc644f.netlify.app',
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 };
