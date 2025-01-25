e36e3dfb7f6b117727c1fa5051ec8c0dfe616260
Gabby <igabrieloyebanji@protonmail.com>
2025-01-25 20:16:12 +0100

    CHECK THIS COMMIT OUT

--- netlify/functions/my-function.js
+++ netlify/functions/my-function.js
@@ -1,9 +1,11 @@
 // netlify/functions/my-function.js
-exports.handler = async (event, context) => {
+exports.handler = async (req, res) => {
     // const apiKey = process.env.MY_API_KEY;
-    return {
-        statusCode: 200,
-        // body: JSON.stringify({ message: `Hello from Netlify Function! \nYour API Key: ${apiKey}` }),
-        body: JSON.stringify({ message: 'Hello from Netlify Function!' }),
-    };
+    // return {
+    //     statusCode: 200,
+    //     // body: JSON.stringify({ message: `Hello from Netlify Function! \nYour API Key: ${apiKey}` }),
+    //     body: JSON.stringify({ message: 'Hello from Netlify Function!' }),
+    // };
+
+    return res.redirect(302, "/*");
 };
