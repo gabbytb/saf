0ce65d61ea5ed8f2573ea028308f9f949cbd9bc5
Gabby <igabrieloyebanji@protonmail.com>
2025-01-27 00:05:44 +0100

    CHECK THIS COMMIT OUT

--- server/app.js
+++ server/app.js
@@ -32,17 +32,6 @@ const app = express();
 // Enable: CORS (CROSS ORIGIN RESOURCE SHARING) for all routes
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.use(cors());
-app.use((req, res, next) => {
-
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with your Netlify domain
-  
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  
-    next();
-  
-});
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // express.json():-  Will add a body property to the request or req object. 
 // - This includes the request body's parsed JSON data. 
@@ -54,10 +43,10 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // Serve static files from the React app (build folder)
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // USE IF:-  client directory is inside server directory.
-// const buildPath = path.join(__dirname, 'client/build')
+const buildPath = path.join(__dirname, 'client/build')
 // - OR -
 // USE IF:-  client directory and server directory are seperate.
-const buildPath = path.join(__dirname, '..', 'client', 'build');
+// const buildPath = path.join(__dirname, '..', 'client', 'build');
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.use(express.static(buildPath));
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
