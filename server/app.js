d5d3c4261edbdc923678ecc51ef29f8187ed2686
Gabby <igabrieloyebanji@protonmail.com>
2025-01-27 00:03:47 +0100

    CHECK THIS COMMIT OUT

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ const app = express();
 app.use(cors());
 app.use((req, res, next) => {
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://67964cb165d323de7e4df4f4--superlative-crepe-cc644f.netlify.app'); // Replace with your Netlify domain
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with your Netlify domain
   
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   
