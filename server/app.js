11ceb85fbc3d7fd1c697ff77d46b3d5e5214c9e6
Gabby <igabrieloyebanji@protonmail.com>
2025-01-27 08:10:34 +0100

    CHECK THIS COMMIT OUT

--- server/app.js
+++ server/app.js
@@ -26,12 +26,11 @@ const app = express();
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Enable: CORS (CROSS ORIGIN RESOURCE SHARING) for all routes
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const corsOptions = {
-    origin: "https://67964cb165d323de7e4df4f4--superlative-crepe-cc644f.netlify.app/",    // Set origin of client-side IP 
-    credentials: true,            // access-control-allow-credentials:true
-    optionSuccessStatus: 200
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
 };
-app.use(cors());
 app.use(cors(corsOptions));
 // Handle preflight CORS request
 // app.options('*', cors(corsOptions));
