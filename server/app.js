7a31651a204820a83b63709f7b0167a496159bc4
Gabby <igabrieloyebanji@protonmail.com>
2025-01-27 20:32:55 +0100

    COMMIT NEW

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ const app = express();
 // Enable: CORS (CROSS ORIGIN RESOURCE SHARING) for all routes
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const corsOptions = {
-    origin: "http://localhost:8888",
+    origin: ip,
     methods: 'GET, POST, PUT, DELETE',  // Specify which methods are allowed
     allowedHeaders: 'Content-Type,Authorization', // Specify which headers are allowed
     credentials: true,  // Allows cookies and access-control-allow-credentials to be sent with the request
