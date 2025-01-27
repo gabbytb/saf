b450729b400eb2ceab1f7687c47c75c221951d7e
Gabby <igabrieloyebanji@protonmail.com>
2025-01-27 17:42:33 +0100

    COMMIT NOW

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ const corsOptions = {
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 };
-app.use(cors());
+app.use(cors(corsOptions));
 // Handle preflight CORS request
 // app.options('*', cors(corsOptions));
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
