041b4889c3b57dba475104406b6b962d4de9ea9c
Gabby <igabrieloyebanji@protonmail.com>
2025-01-27 17:17:01 +0100

    COMMIT NOW

--- server/app.js
+++ server/app.js
@@ -27,11 +27,11 @@ const app = express();
 // Enable: CORS (CROSS ORIGIN RESOURCE SHARING) for all routes
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const corsOptions = {
-    origin: "https://6797ab6ce1da87aedf337341--samuelakinolafoundation.netlify.app",
+    origin: "samuelakinolafoundation.netlify.app",
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 };
-app.use(cors());
+app.use(cors(corsOptions));
 // Handle preflight CORS request
 // app.options('*', cors(corsOptions));
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
