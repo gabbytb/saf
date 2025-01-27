2d3d5e404e6ba8dbbdbc212f8a90f78c1122f7ed
Gabby <igabrieloyebanji@protonmail.com>
2025-01-27 20:40:36 +0100

    COMMIT NEW

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ const app = express();
 const corsOptions = {
     origin: '*',
     methods: 'GET, POST, PUT, DELETE',  // Specify which methods are allowed
-    allowedHeaders: 'Content-Type,Authorization', // Specify which headers are allowed
+    allowedHeaders: 'Content-Type, Authorization', // Specify which headers are allowed
     credentials: true,  // Allows cookies and access-control-allow-credentials to be sent with the request
 };
 app.use(cors(corsOptions));
