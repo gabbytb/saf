f05091ce68599b4470d1470d5c1e030f3a9e86c2
Gabby <igabrieloyebanji@protonmail.com>
2025-01-27 20:31:20 +0100

    COMMIT NEW

--- client/src/api.js
+++ client/src/api.js
@@ -9,8 +9,8 @@ import axios from 'axios';
 
 // Create an instance of Axios with default settings
 const api = axios.create({    
-    baseURL: "http://localhost:8000",   // This is your server’s base URL
-    // baseURL: "https://6797aeef2b8b49007fd003b1--samuelakinolafoundation.netlify.app",     // This is your server’s base URL
+    // baseURL: "http://localhost:8000",   // This is your server’s base URL
+    baseURL: "https://samuelakinolafoundation.netlify.app",     // This is your server’s base URL
     timeout: 20000, // Optional: Maximum time to wait for a response
     // headers: {
     //     'Content-Type': 'application/json',
