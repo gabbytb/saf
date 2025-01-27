6557df1a61b014697265276ded1f5a9937784178
Gabby <igabrieloyebanji@protonmail.com>
2025-01-27 16:59:49 +0100

    CHECK THIS COMMIT OUT

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ const corsOptions = {
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 };
-app.use(cors(corsOptions));
+app.use(cors());
 // Handle preflight CORS request
 // app.options('*', cors(corsOptions));
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
