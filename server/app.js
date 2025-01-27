b4d5fd630b3d6bf7d545175fa499a55bc19ab67b
Gabby <igabrieloyebanji@protonmail.com>
2025-01-27 08:42:30 +0100

    CHECK THIS COMMIT OUT

--- server/app.js
+++ server/app.js
@@ -26,8 +26,8 @@ const app = express();
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Enable: CORS (CROSS ORIGIN RESOURCE SHARING) for all routes
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const corsOptions ={
-    origin:'https://67964cb165d323de7e4df4f4--superlative-crepe-cc644f.netlify.app', 
+const corsOptions = {
+    origin:'https://67964cb165d323de7e4df4f4--samuelakinolafoundation.netlify.app',
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 };
