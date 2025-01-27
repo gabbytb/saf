61ef81b0b291cec1360a6bc31809e703eed22faa
Gabby <igabrieloyebanji@protonmail.com>
2025-01-27 18:09:19 +0100

    COMMIT NEW

--- netlify/functions/function.js
+++ netlify/functions/function.js
@@ -5,6 +5,10 @@ exports.handler = async (event, context) => {
     
     return {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*', // Allow all origins (or replace '*' with your frontend domain)
+            'Content-Type': 'application/json',
+        },
         // body: JSON.stringify({ message: `Hello from Netlify Function! \nYour API Key: ${apiKey}` }),
         body: JSON.stringify({ message: 'Hello from Netlify Function!' }),
     };
