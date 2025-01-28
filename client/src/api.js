369c12b45c10a1d4c6da38db8dbc8a326d32b8db
Gabby <igabrieloyebanji@protonmail.com>
2025-01-28 18:27:09 +0100

    FIX CORS ISSUE WITH NETLIFY TO WORK FROM NETLIFY DOMAIN URL

--- client/src/api.js
+++ client/src/api.js
@@ -14,9 +14,8 @@ import axiosRetry from "axios-retry";
 
 // Create an instance of Axios with default settings
 const api = axios.create({
-                // baseURL: "https://192.168.205.113:8000",   // This is your server’s base URL            
-                baseURL: "https://lemon-houses-judge.loca.lt",   // This is your server’s base URL            
-                // baseURL: "https://samuelakinolafoundation.netlify.app",   // This is your server’s base URL            
+                // baseURL: "https://192.168.205.113:8000",   // LOCAL Domains:- This is your server’s base URL            
+                baseURL: "https://lemon-houses-judge.loca.lt",   // HTTPS Domains:- This is your server’s base URL
                 timeout: 20000, // Optional: Maximum time to wait for a response                
             });
 
