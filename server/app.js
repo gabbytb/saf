388a6093cf4108ef59f22f1ca4a93c9127cb2d3d
Gabby <igabrieloyebanji@protonmail.com>
2025-01-28 15:26:53 +0100

    FIX CORS ISSUE WITH NETLIFY TO WORK FROM NETLIFY DOMAIN URL

--- server/app.js
+++ server/app.js
@@ -40,54 +40,41 @@ const credentials = { key: privateKey, cert: certificate };
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Enable: CORS (CROSS ORIGIN RESOURCE SHARING) for all routes
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// SET THE URL OF THE FRONTEND HERE (i.e Web Browser url = http://localhost:3000)
-// const corsOptions = {
-//     origin: 'http://192.168.138.113:3000',
-//     methods: 'GET, POST, PUT, DELETE',  // Specify which methods are allowed
-//     allowedHeaders: 'Content-Type, Authorization', // Specify which headers are allowed
-//     credentials: true,  // Allows cookies and access-control-allow-credentials to be sent with the request
-// };
+// SET THE URL OF DOMAINS THAT ARE GOING TO MAKE REQUEST FROM FRONTEND HERE (i.e Web Browser url = https://samuelakinolafoundation.com)
 // Your list of allowed origins (domains)
-// const allowedOrigins = [
-//     "https://samuelakinolafoundation.netlify.app", // Allow production frontend on Netlify / Allow the frontend domain
-//     "https://localhost:3000",  // Allow local React app
-//     "https://192.168.234.113:3000",  // Allow local network access if needed
-// ];
+const allowedOrigins = [
+    "https://samuelakinolafoundation.netlify.app", // Allow production frontend on Netlify / Allow the frontend domain
+    "https://localhost:3000",  // Allow local React app
+    "https://192.168.234.113:3000",  // Allow local network access if needed
+];
 
-// // Configure CORS options
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         // If the origin is in allowedOrigins or it's empty (e.g. Postman, or curl), allow the request
-//         if (allowedOrigins.includes(origin) || !origin) {  // origin can be null for Postman or CURL requests
-//             callback(null, true);  // Allow the request
-//         } else {
-//             callback(new Error('Not allowed by CORS'));  // Block the request if origin isn't allowed
-//         };
-//     },
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
-//     allowedHeaders: ['Content-Type', 'Authorization'], // Allow headers for requests
-//     credentials: true,  // Don't pass cookies by default
-    
-//     // Setting the withCredentials option to true
-//     // axios.get('https://your-api-url.com/endpoint', {
-//     //     withCredentials: true  // Tells Axios to send cookies along with the request
-//     // })
-  
-// };
-
-// CORS configuration for accepting credentials
+// Configure CORS options
 const corsOptions = {
-    origin: 'https://samuelakinolafoundation.netlify.app', // Your frontend domain (this is crucial)
-    methods: ['GET', 'POST'], // Allowed methods, adjust as necessary
-    credentials: true, // Allow credentials (cookies, Authorization headers, etc.)
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers for requests
+    origin: (origin, callback) => {
+        // If the origin is in allowedOrigins or it's empty (e.g. Postman, or curl), allow the request
+        if (allowedOrigins.includes(origin) || !origin) {  // origin can be null for Postman or CURL requests
+            callback(null, true);  // Allow the request
+        } else {
+            callback(new Error('Not allowed by CORS'));  // Block the request if origin isn't allowed
+        };
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods    
+    credentials: true, // CORS configuration for accepting credentials (cookies, Authorization headers, etc.)
+    // allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers for requests
+    allowedHeaders: 'Content-Type', // Specify which headers are allowed
+    
+    // Setting the withCredentials option to true
+    // axios.get('https://your-api-url.com/endpoint', {
+    //     withCredentials: true  // Tells Axios to send cookies along with the request
+    // })  
 };
+
 // Apply CORS settings to the Express app
 // Now your Express server will allow requests from these three locations and respond without CORS issues.
 app.use(cors(corsOptions));
 
 // Handle preflight CORS request
-// app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions));
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // express.json():-  Will add a body property to the request or req object. 
 // - This includes the request body's parsed JSON data. 
