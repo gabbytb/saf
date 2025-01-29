9e10ea23056a1d79145d50f0596bd576ad5b1715
Gabby <igabrieloyebanji@protonmail.com>
2025-01-29 14:45:32 +0100

    FIX CORS ISSUE WITH NETLIFY TO WORK FROM NETLIFY DOMAIN URL

--- client/src/api.js
+++ client/src/api.js
@@ -15,8 +15,12 @@ import axiosRetry from "axios-retry";
 // Create an instance of Axios with default settings
 const api = axios.create({
                 // baseURL: "https://192.168.205.113:8000",   // LOCAL Domains:- This is your server’s base URL            
-                baseURL: "https://orange-mirrors-wish.loca.lt",   // HTTPS Domains:- This is your server’s base URL
+                baseURL: "https://famous-rabbits-wash.loca.lt",   // HTTPS Domains:- This is your server’s base URL
                 timeout: 20000, // Optional: Maximum time to wait for a response                
+                // withCredentials: true, // If cookies or sessions are needed
+                // headers: {
+                //   'Content-Type': 'application/json',
+                // },
             });
 
             // Enable retries with axios
