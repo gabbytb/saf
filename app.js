2b98dde469bec34ed357d4b5379af03862c19232
Gabby <igabrieloyebanji@protonmail.com>
2025-01-30 15:11:45 +0100

    SUCCESSFUL COMMIT

--- app.js
+++ app.js
@@ -73,10 +73,8 @@ const credentials = {
 // DEVELOPMENT: Your list of allowed origins (domains) (i.e https://samuelakinolafoundation.netlify.app).
 const allowedOrigins = [
     "https://samuelakinolafoundation.netlify.app", // Allow production frontend on Netlify / Allow the frontend domain
-    "https://localhost:3000",  // Allow local React app
-    "https://192.168.205.113:3000",  // Allow local network access if needed
-    "http://localhost:8888",  // Allow local React app
-    "http://192.168.205.113:8888",  // Allow local network access if needed
+    "https://localhost:8000",  // Allow local React app
+    "https://192.168.205.113:8000",  // Allow local network access if needed
 ];
 // Configure CORS options
 const corsOptions = {
