779772de6335e55bf6570f934f838c1f4f199b2e
Gabby <igabrieloyebanji@protonmail.com>
2025-01-30 15:36:29 +0100

    SUCCESSFUL COMMIT

--- client/src/api.js
+++ client/src/api.js
@@ -16,7 +16,7 @@ import axiosRetry from "axios-retry";
 const api = axios.create({
                 // baseURL: "https://localhost:8000",   // LOCAL Domains:- This is your server’s base URL
                 // baseURL: "https://192.168.238.113:8000",   // LOCAL Domains:- This is your server’s base URL            
-                baseURL: "https://fifty-mammals-mate.loca.lt",   // HTTPS Domains:- This is your server’s base URL
+                baseURL: "https://lovely-pans-drive.loca.lt",   // HTTPS Domains:- This is your server’s base URL
                 timeout: 20000, // Optional: Maximum time to wait for a response                
                 // withCredentials: true, // If cookies or sessions are needed
                 // headers: {
