ec7f500d0d32e032b21b06b24334ad203f3a4a95
Gabby <igabrieloyebanji@protonmail.com>
2025-02-01 10:19:53 +0100

    SUCCESSFUL COMMIT

--- client/src/api.js
+++ client/src/api.js
@@ -15,8 +15,8 @@ import axiosRetry from "axios-retry";
 // Create an instance of Axios with default settings
 const api = axios.create({
                 // baseURL: "https://localhost:8000",   // LOCAL Domains:- This is your server’s base URL
-                baseURL: "https://192.168.40.113:8000",   // LOCAL Domains:- This is your server’s base URL            
-                // baseURL: "https://samuelakinolafoundation.netlify.app",   // HTTPS Domains:- This is your server’s base URL
+                // baseURL: "https://192.168.40.113:8000",   // LOCAL Domains:- This is your server’s base URL            
+                baseURL: "https://samuelakinolafoundation.netlify.app",   // HTTPS Domains:- This is your server’s base URL
                 timeout: 20000, // Optional: Maximum time to wait for a response                
                 // withCredentials: true, // If cookies or sessions are needed
                 headers: {
--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-    // "https://samuelakinolafoundation.netlify.app", // Allow production frontend on Netlify / Allow the frontend domain    
+    "https://samuelakinolafoundation.netlify.app", // Allow production frontend on Netlify / Allow the frontend domain    
     // "https://localhost:3000",  // Allow local React app
-    "https://192.168.40.113:3000",  // Allow local network access if needed
+    // "https://192.168.40.113:3000",  // Allow local network access if needed
 ];
 
 module.exports = allowedOrigins;
