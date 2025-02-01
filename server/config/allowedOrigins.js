ea8ae1dd9d9cb392ff06e80af468a278a3efb5ee
Gabby <igabrieloyebanji@protonmail.com>
2025-02-01 20:14:25 +0100

    READY TO DEPLOY

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     // "https://samuelakinolafoundation.netlify.app", // Allow production frontend on Netlify / Allow the frontend domain    
-    "https://localhost:3000",  // Allow local React app
+    // "https://localhost:3000",  // Allow local React app
     "https://192.168.124.113:3000",  // Allow local network access if needed
 
 
