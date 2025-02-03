a14b6b4b9680b7130ee067cb6de4ed678b7662a7
Gabby <igabrieloyebanji@protonmail.com>
2025-02-03 16:42:43 +0100

    READY TO DEPLOY

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     // "https://localhost:3000",  // Allow local React app
     // "https://192.168.204.113:3000",  // Allow local network access if needed
     "https://saf-ea7m.onrender.com", // Allow production frontend on Render / Allow the frontend domain    
+    "https://saf-api.onrender.com",
 
     // 'http://localhost:3000',
     // 'https://www.dandrepairshop.com',
